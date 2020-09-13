714bce414fec71b9b1e350e54ce0b2100a094236
chaitanya-suvarna <Chaitanya.svrn@gmail.com>
2020-09-13 13:30:01 +0530

    Service and Storage pages added

--- RemoteServerStatus/Pages/Index.cshtml.cs
+++ RemoteServerStatus/Pages/Index.cshtml.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
@@ -17,9 +13,9 @@ namespace RemoteServerStatus.Pages
             _logger = logger;
         }
 
-        public void OnGet()
+        public IActionResult OnGet()
         {
-
+            return RedirectToPage("./Service");
         }
     }
 }
---
+++ RemoteServerStatus/Pages/Storage.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RemoteServerStatus.Pages
+{
+    public class StorageModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
