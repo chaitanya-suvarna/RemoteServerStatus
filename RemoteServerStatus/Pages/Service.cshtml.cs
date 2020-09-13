92e3e567bc44e85490ec96228c319667c3477320
chaitanya-suvarna <Chaitanya.svrn@gmail.com>
2020-09-13 13:57:24 +0530

    Added storage info page

--- RemoteServerStatus/Pages/Service.cshtml.cs
+++ RemoteServerStatus/Pages/Service.cshtml.cs
@@ -1,18 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using RemoteServerStatus.Services;
+using System.Collections.Generic;
 
 namespace RemoteServerStatus.Pages
 {
     public class ServiceModel : PageModel
     {
         private readonly ILogger<ServiceModel> _logger;
-        private List<WindowsService> windowsServiceList;
+        public List<WindowsService> windowsServiceList;
         private readonly IManagementService _managementService;
 
         public ServiceModel(ILogger<ServiceModel> logger, IManagementService managementService)
--- RemoteServerStatus/Pages/Storage.cshtml.cs
+++ RemoteServerStatus/Pages/Storage.cshtml.cs
@@ -1,17 +1,26 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using RemoteServerStatus.Services;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RemoteServerStatus.Pages
 {
     public class StorageModel : PageModel
     {
+        private readonly ILogger<StorageModel> _logger;
+        public List<StorageDetails> storageDetailsList;
+        private readonly IManagementService _managementService;
+
+        public StorageModel(ILogger<StorageModel> logger, IManagementService managementService)
+        {
+            _logger = logger;
+            storageDetailsList = new List<StorageDetails>();
+            _managementService = managementService;
+        }
+
         public void OnGet()
         {
-
+            storageDetailsList = _managementService.FetchStorageData();
         }
     }
 }
